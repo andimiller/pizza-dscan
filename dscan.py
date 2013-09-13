569c4f5bf1be3560607e9746766c6d298d05d4f7
Andi Miller <andi@andimiller.net>
2013-09-13 20:00:47 +0100

    added a load of pretty bars

--- dscan.py
+++ dscan.py
@@ -2,16 +2,23 @@ import sqlite3
 import re
 import datetime
 import json
+import redis_wrap
 
 isinteresting = lambda x:x["group"] is not None
 
 class DScan():
-	def __init__(self, redisdb):
-		self.redisdb = redisdb
+	def __init__(self):
+		self.redisdb = redis_wrap.get_hash('dscan_categories')
+		self.redis_sizes = redis_wrap.get_hash('dscan_sizes')
+		self.redis_systems = redis_wrap.get_hash('dscan_systems')
 		self.db = sqlite3.connect('ody11-sqlite3-v1.db')
 		c = self.db.cursor()
 		for row in c.execute("select distinct invTypes.typeName, invGroups.groupName, invCategories.categoryName from invTypes, invGroups, invCategories WHERE invGroups.groupID=invTypes.groupID AND invCategories.categoryID=invGroups.categoryID"):
 			self.redisdb[row[0]] = json.dumps((row[1] or None, row[2] or None))
+		for row in c.execute("select invTypes.typeName, dgmTypeAttributes.valueFloat from invTypes, dgmTypeAttributes where dgmTypeAttributes.typeID=invTypes.typeID and dgmTypeAttributes.attributeID=1547"):
+			self.redis_sizes[row[0]] = int(row[1])
+		for row in c.execute("select mapRegions.regionName, mapSolarSystems.solarSystemName from mapRegions, mapSolarSystems where mapRegions.regionID=mapSolarSystems.regionID"):
+			self.redis_systems[row[1]] = row[0]
 		c.close()
 		print "Database loaded"
 
@@ -25,6 +32,66 @@ class DScan():
 		result["category"] = str(group) and group[1] or None
 		return result
 
+	def buildsizes(self, data):
+		sizes = {
+				"Frigates": {},
+				"Destroyers": {},
+				"Cruisers": {},
+				"Battlecruisers": {},
+				"Battleships": {},
+				"Capitals": {},
+				"Supercapitals": {}
+		}
+		for line in data:
+			typen = line["type"]
+			category = line["category"]
+			group = line["group"]
+			if line["type"] not in self.redis_sizes:
+				continue
+			rigsize = int(self.redis_sizes[line["type"]])
+			if rigsize==1:
+				if group in ["Inderdictor", "Destroyer"]:
+					if typen in sizes["Destroyers"]:
+						sizes["Destroyers"][typen]+=1
+					else:
+						sizes["Destroyers"][typen]=1
+				else:
+					if typen in sizes["Frigates"]:
+						sizes["Frigates"][typen]+=1
+					else:
+						sizes["Frigates"][typen]=1
+			if rigsize==2:
+				if group in ["Battlecruiser", "Attack Battlecruiser", "Command Ship"]:
+					if typen in sizes["Battlecruisers"]:
+						sizes["Battlecruisers"][typen]+=1
+					else:
+						sizes["Battlecruisers"][typen]=1
+				else:
+					if typen in sizes["Cruisers"]:
+						sizes["Cruisers"][typen]+=1
+					else:
+						sizes["Cruisers"][typen]=1
+			if rigsize==3:
+				if typen in sizes["Battleships"]:
+					sizes["Battleships"][typen]+=1
+				else:
+					sizes["Battleships"][typen]=1
+			if rigsize==4:
+				if group in ["Titan", "Supercarrier"]:
+					if typen in sizes["Supercapitals"]:
+						sizes["Supercapitals"][typen]+=1
+					else:
+						sizes["Supercapitals"][typen]=1
+				else:
+					if typen in sizes["Capitals"]:
+						sizes["Capitals"][typen]+=1
+					else:
+						sizes["Capitals"][typen]=1
+		return sizes
+
+
+
+
 	def total(self, data):
 		categorywide = {}
 		sums = {}
@@ -34,23 +101,26 @@ class DScan():
 			typen = line["type"]
 			category = line["category"]
 			group = line["group"]
+			# large categories
 			if category not in categorywide:
 				categorywide[category] = {}
 			if typen in categorywide[category]:
 				categorywide[category][typen] += 1
 			else:
 				categorywide[category][typen] = 1
+			# sums
 			if category not in sums:
 				sums[category] = 1
 			else:
 				sums[category] += 1
+			# groups
 			if group not in groups:
 				groups[group] = {}
 			if typen in groups[group]:
 				groups[group][typen] += 1
 			else:
 				groups[group][typen]=1
-
+			# categories of groups
 			if category not in categories:
 				categories[category] = {}
 			if group in categories[category]:
@@ -107,10 +177,21 @@ class DScan():
 		data = map(lambda x:x.split("\t"), data.split("\n"))
 		data = filter(lambda x:len(x)==3 , data)
 		data = map(self.categorise, data)
-		systemname = self.getsystemname(data)
+		systemname=self.getsystemname(data)
+		if systemname:
+			region = self.redis_systems[systemname]
+			systemname = (systemname, region)
 		data = filter(isinteresting, data)
 		categorywide, sums, categories, groups = self.total(data)
 		categories = self.sort(categories)
 		groups = self.sort(groups)
 		categorywide = self.sort(categorywide)
-		return categorywide, sums, categories, groups, systemname
+		sizes = self.buildsizes(data)
+		sizestotal = 0
+		for size in sizes:
+			sizestotal+=sum(sizes[size].values())
+
+
+		results = {"categorywide" : categorywide, "sums" : sums, "categories" : categories, "groups": groups, "system" : systemname, "sizemap": sizes, "sizestotal": sizestotal}
+
+		return json.dumps(results)
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import json
 from flask import Flask, flash, session, render_template, redirect, request, abort, url_for, escape
 import redis_wrap
-
-from dscan import DScan
 import string, random
+import hashlib
+from string import zfill
+from dscan import DScan
 
 app = Flask(__name__)
 
@@ -13,7 +14,7 @@ with open("config.json") as fh:
 assert(config)
 app.config.update(config)
 
-dscan = DScan(redis_wrap.get_hash('dscan_categories'))
+dscan = DScan()
 store = redis_wrap.get_hash('dscan')
 
 @app.route('/', methods=["GET", "POST"])
@@ -21,29 +22,52 @@ def dscanmake():
 	if request.method=="GET":
 		return render_template("dscan.html")
 	if request.method=="POST":
-		categorywide, sums, categories, groups, system = dscan.parseDscan(request.form["dscandata"])
+		data = dscan.parseDscan(request.form["dscandata"])
 		token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(24))
 		while token in store:
 			token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(24))
-		store[token] = json.dumps({"categorywide" : categorywide, "sums" : sums, "categories" : categories, "groups": groups, "system" : system})
+		store[token] = data
 		return redirect("/view/"+token)
+
+
+def color_variant(hex_color, brightness_offset=1):
+	""" takes a color like #87c95f and produces a lighter or darker variant """
+	if len(hex_color) != 7:
+		raise Exception("Passed %s into color_variant(), needs to be in #87c95f format." % hex_color)
+	rgb_hex = [hex_color[x:x+2] for x in [1, 3, 5]]
+	new_rgb_int = [int(hex_value, 16) + brightness_offset for hex_value in rgb_hex]
+	new_rgb_int = [min([255, max([0, i])]) for i in new_rgb_int] # make sure new values are between 0 and 255
+	# hex() produces "0x88", we want just "88"
+	return "#" + "".join([hex(i)[2:] for i in new_rgb_int])
+
+def hashstringtocolor(s):
+	base = (0, 0, 50)
+	md5 = hashlib.md5()
+	md5.update(s)
+	digest = md5.hexdigest()
+	r = int(digest[0:10], 16) % 256
+	g = int(digest[10:20], 16) % 256
+	b = int(digest[20:30], 16) % 256
+	# mix with base
+	r = (r + base[0]) / 2
+	g = (g + base[1]) / 2
+	b = (b + base[2]) / 2
+	return '#{:02x}{:02x}{:02x}'.format(r,g,b)
 
 @app.route('/view/<id>')
 def dscanview(id):
 	data = json.loads(store[id])
-	categorywide = data["categorywide"]
-	sums = data["sums"]
-	categories = data["categories"]
-	groups = data["groups"]
-	system = data["system"]
-	categorylist = categories.keys()
-	print categorylist
-	if "Ship" in categorylist:
-		categorylist.remove("Ship")
-		categorylist = ["Ship"] + sorted(categorylist)
+	data["categorylist"] = data["categories"].keys()
+	if "Ship" in data["categorylist"]:
+		data["categorylist"].remove("Ship")
+		data["categorylist"] = ["Ship"] + sorted(data["categorylist"])
 	else:
-		categorylist = sorted(categorylist)
-	return render_template("dscan_results.html", categorylist=categorylist, categorywide=categorywide, sums=sums, categories=categories, groups=groups, system=system)
+		data["categorylist"] = sorted(data["categorylist"])
+	data["sizeorder"] = data["sizemap"].keys()
+	data["sizeorder"] = sorted(data["sizeorder"], key=lambda x: -sum(data["sizemap"][x].values()))
+	data["color_variant"] = color_variant
+	data["string2color"] = hashstringtocolor
+	return render_template("dscan_results_new.html", **data)
 
 
 @app.teardown_appcontext
